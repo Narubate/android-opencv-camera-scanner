721ece8384d27c71e0455316d8d200f7a5456601
Narubate Jamsai <>
2019-11-08 13:38:23 +0700

    Add button take camera

--- app/src/main/java/id/aashari/code/camerascanner/MainActivity.java
+++ app/src/main/java/id/aashari/code/camerascanner/MainActivity.java
@@ -4,19 +4,28 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import id.aashari.code.camerascanner.helpers.MyConstants;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnOpenCamera;
     Button btnOpenGallery;
     Button btnImageProcess;
 
@@ -34,12 +43,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initializeElement() {
-        this.imageView = (ImageView) findViewById(R.id.imageView);
-        this.btnOpenGallery = (Button) findViewById(R.id.btnOpenGallery);
-        this.btnImageProcess = (Button) findViewById(R.id.btnImageProcess);
+        this.imageView = findViewById(R.id.imageView);
+        this.btnOpenCamera = findViewById(R.id.btnOpenCamera);
+        this.btnOpenGallery = findViewById(R.id.btnOpenGallery);
+        this.btnImageProcess = findViewById(R.id.btnImageProcess);
     }
 
     private void initializeEvent() {
+        this.btnOpenCamera.setOnClickListener(this.btnOpenCameraClick);
         this.btnOpenGallery.setOnClickListener(this.btnOpenGalleryClick);
         this.btnImageProcess.setOnClickListener(this.btnImageProcessClick);
     }
@@ -50,6 +61,25 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(Intent.ACTION_PICK);
             intent.setType("image/*");
             startActivityForResult(intent, MyConstants.GALLERY_IMAGE_LOADED);
+        }
+    };
+
+    private View.OnClickListener btnOpenCameraClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            File file = createImageFile();
+            boolean isDirectoryCreated = file.getParentFile().mkdirs();
+            Log.d("", "openCamera: isDirectoryCreated: " + isDirectoryCreated);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                Uri tempFileUri = FileProvider.getUriForFile(getApplicationContext(), getApplicationContext().getPackageName() + ".provider", // As defined in Manifest
+                        file);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, tempFileUri);
+            } else {
+                Uri tempFileUri = Uri.fromFile(file);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, tempFileUri);
+            }
+            startActivityForResult(cameraIntent, MyConstants.CAMERA_LOADED);
         }
     };
 
@@ -72,9 +102,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == MyConstants.GALLERY_IMAGE_LOADED && resultCode == RESULT_OK && data != null) {
-            selectedImage = data.getData();
-            this.loadImage();
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case MyConstants.CAMERA_LOADED:
+                    this.loadImage();
+                    break;
+                case MyConstants.GALLERY_IMAGE_LOADED:
+                    selectedImage = data.getData();
+                    this.loadImage();
+                    break;
+            }
+        } else {
+            Toast.makeText(this, "Result not found.", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -83,10 +122,29 @@ public class MainActivity extends AppCompatActivity {
             InputStream inputStream = getContentResolver().openInputStream(this.selectedImage);
             selectedBitmap = BitmapFactory.decodeStream(inputStream);
             imageView.setImageBitmap(selectedBitmap);
-            btnImageProcess.setVisibility(View.VISIBLE);
+            btnImageProcess.setEnabled(true);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
+    private File createImageFile() {
+        clearTempImages();
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new
+                Date());
+        File file = new File(MyConstants.IMAGE_PATH, "IMG_" + timeStamp +
+                ".jpg");
+        selectedImage = Uri.fromFile(file);
+        return file;
+    }
+
+    private void clearTempImages() {
+        try {
+            File tempFolder = new File(MyConstants.IMAGE_PATH);
+            for (File f : tempFolder.listFiles())
+                f.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/id/aashari/code/camerascanner/helpers/MyConstants.java
+++ app/src/main/java/id/aashari/code/camerascanner/helpers/MyConstants.java
@@ -1,8 +1,12 @@
 package id.aashari.code.camerascanner.helpers;
 
 import android.graphics.Bitmap;
+import android.os.Environment;
 
 public class MyConstants {
-    public final static int GALLERY_IMAGE_LOADED = 1001;
+    public final static int CAMERA_LOADED = 1001;
+    public final static int GALLERY_IMAGE_LOADED = 1002;
     public static Bitmap selectedImageBitmap;
+    public final static String IMAGE_PATH = Environment
+            .getExternalStorageDirectory().getPath() + "/CamScannerSample";
 }
